859b37a50e8d2be1950933eefbc7f613f67827ca
mtwtkman <924277+mtwtkman@users.noreply.github.com>
2023-08-09 21:58:30 +0900

    add feature

--- content-script.js
+++ content-script.js
@@ -1,6 +1,36 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    document.querySelector("video").playbackRate = request.changePlaybackRate;
+    switch (request.feature) {
+      case "change-playbackrate":
+        changePlaybackRate(request.rate);
+        break;
+      case "manipulate-pip":
+        manipulatePip(request.manipulationType);
+        break;
+      default:
+        throw "unknown feature";
+    }
     sendResponse({done: true});
   }
 );
+
+function findVideo() {
+  return document.querySelector("video");
+}
+
+function changePlaybackRate(rate) {
+    findVideo().playbackRate = rate;
+}
+
+function manipulatePip(manipulationType) {
+  switch (manipulationType) {
+    case "request":
+      findVideo().requestPictureInPicture();
+      break;
+    case "exit":
+      document.exitPictureInPicture();
+      break;
+    default:
+      throw "unknown pip manipulation type";
+  }
+}
--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,120 @@
-const rates = [
-  0.1,
-  0.25,
-  0.5,
-  1,
-  1.5,
-  2,
-  2.5,
-  3,
-  3.5,
-  4,
-  4.5,
-  5,
-];
+const changePlaybackRateFeature = {
+  id: "change-playbackrate",
+  title: "Change playbackrate",
+  context: [
+    0.1,
+    0.25,
+    0.5,
+    1,
+    1.5,
+    2,
+    2.5,
+    3,
+    3.5,
+    4,
+    4.5,
+    5,
+  ],
+};
 
-chrome.contextMenus.onClicked.addListener(changePlaybackRate);
+const manipulatePipFeature = {
+  id: "manipulate-pip",
+  title: "Manipulate pip",
+  context: [
+    "request",
+    "exit",
+  ],
+};
 
-async function changePlaybackRate(info) {
-  const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true });
-  const rate = parseFloat(info.menuItemId);
-  const resp = await chrome.tabs.sendMessage(tab.id, { changePlaybackRate: rate });
+const CHILD_MENU_ITEM_ID_DELIMITER = ":";
+
+function buildChildMenuItemIdPrefix(feature) {
+  return `${feature.id}${CHILD_MENU_ITEM_ID_DELIMITER}`
+}
+
+function throwUnknownFeatureError() {
+  throw "unknown feature"
+}
+
+function detectFeature(menuItemId) {
+  if (menuItemId.startsWith(changePlaybackRateFeature.id)) {
+    return changePlaybackRateFeature;
+  } else if (menuItemId.startsWith(manipulatePipFeature.id)) {
+    return manipulatePipFeature;
+  }
+  throwUnknownFeatureError();
+}
+
+async function performFeature(tab, feature, menuItemId) {
+  switch (feature.id) {
+    case changePlaybackRateFeature.id:
+      return await performChangePlaybackRate(tab, menuItemId)
+      break;
+    case manipulatePipFeature.id:
+      return await performManipulatePip(tab, menuItemId)
+      break;
+    default:
+      throwUnknownFeatureError();
+  }
+}
+
+async function trigger(info) {
+  const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+  const feature = detectFeature(info.menuItemId);
+  const resp = await performFeature(tab, feature, info.menuItemId)
   console.log(resp);
 }
 
-chrome.runtime.onInstalled.addListener(function () {
-  rates.forEach(rate => {
-    const s = JSON.stringify(rate);
+function removeFeaturePrefix(feature, value) {
+  return value.replace(new RegExp(buildChildMenuItemIdPrefix(feature)), "");
+}
+
+function menuItemIdToRate(menuItemId) {
+  return parseFloat(removeFeaturePrefix(changePlaybackRateFeature, menuItemId));
+}
+
+async function sendTabMessage(tab, feature, payload) {
+  return await chrome.tabs.sendMessage(tab.id, {feature: feature.id, ...payload});
+}
+
+async function performChangePlaybackRate(tab, menuItemId) {
+  const rate = menuItemIdToRate(menuItemId);
+  return await sendTabMessage(tab, changePlaybackRateFeature, {rate});
+}
+
+function menuItemIdToPipManipulationType(menuItemId) {
+  return removeFeaturePrefix(manipulatePipFeature, menuItemId)
+}
+
+async function performManipulatePip(tab, menuItemId) {
+  const manipulationType = menuItemIdToPipManipulationType(menuItemId);
+  return await sendTabMessage(tab, manipulatePipFeature, {manipulationType});
+}
+
+function buildChildMenuItemId(feature, value) {
+  return `${buildChildMenuItemIdPrefix(feature)}${value}`
+}
+
+function registerFeature(feature) {
+  const parent = chrome.contextMenus.create({
+    title: feature.title,
+    id: feature.id,
+  });
+  feature.context.forEach(item => {
+    const v = `${item}`;
     chrome.contextMenus.create({
-      title: s,
-      id: s,
+      title: v,
+      id: buildChildMenuItemId(feature, v),
+      parentId: parent,
     });
   });
+}
+
+chrome.contextMenus.onClicked.addListener(trigger);
+chrome.runtime.onInstalled.addListener(() => {
+  [
+    changePlaybackRateFeature,
+    manipulatePipFeature,
+  ].forEach(registerFeature);
 });
+
